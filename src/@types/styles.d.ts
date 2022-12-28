3cf99823e354ef2336e7b2b7c0386a6bb4f5a441
Lucas Bercê de Jesus <lucasberce71@gmail.com>
2022-12-28 15:51:59 -0300

    first commit

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components';
+import { defaultTheme as DefaultTheme } from '../styles/themes/default';
+
+type ThemeType = typeof DefaultTheme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
